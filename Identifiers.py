4fbb283d2dd7bdb6d4c2c89ca8056bbec623d972
MGamal <mg68163@gmail.com>
2024-04-16 14:48:12 +0200

    updating some tests

--- Identifiers.py
+++ Identifiers.py
@@ -5,7 +5,7 @@ from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
 from time import sleep
 from typing import Any, Dict
-from Identifiers import desired_caps
+# from Identifiers import desired_caps
 desired_caps = {
   "platformName": "Android",
   "appium:platformVersion": "14",
@@ -20,6 +20,30 @@ xpath_login_email_box="//android.widget.FrameLayout[@resource-id='android:id/con
 
 xpath_password_box="//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"
 xpath_login_continue="//android.widget.Button[@content-desc='Conitnue']"
+
+
+
+#upvote and downvote
+xpath_upvote_34_post='//android.view.View[@content-desc="11h ago Downvote this Every downvote matters... 😔 34 1"]/android.view.View[3]'
+xpath_downvote_1_post='//android.view.View[@content-desc="11h ago Downvote this Every downvote matters... 😔 34 1"]/android.view.View[4]'
+xpath_34_1_post_settings='//android.view.View[@content-desc="11h ago Downvote this Every downvote matters... 😔 34 1"]/android.view.View[2]'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
