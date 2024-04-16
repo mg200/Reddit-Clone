9e1c671fabe1b424424e5bb69386fa96c89d0621
MGamal <mg68163@gmail.com>
2024-04-16 16:15:58 +0200

    updating Identifiers and main.py

--- Identifiers.py
+++ Identifiers.py
@@ -21,6 +21,8 @@ xpath_login_email_box="//android.widget.FrameLayout[@resource-id='android:id/con
 xpath_password_box="//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"
 xpath_login_continue="//android.widget.Button[@content-desc='Conitnue']"
 
+signup_xpath="//android.widget.Button[@content-desc='Sign up']"
+Signup_accessibility_id="Sign up"
 
 
 #upvote and downvote
@@ -49,23 +51,36 @@ logout_accessibility_id="Logout"
 settings_xpath="//android.view.View[@content-desc='Settings']"
 settings_accessibility_id="Settings"
 # 1. account settings
-
-# {
 account_settings_for_mariam_xpath="//android.view.View[@content-desc='Account settings for u/mariam']"
 account_settings_for_mariam_accessibility_id="Account settings for u/mariam"
 
-
+# {
+# 1.1- account settings
 #update email address
 update_email_address_xpath="//android.view.View[@content-desc='Update email address mariamgamal70.backup@gmail.com']"
 update_email_address_accessibility_id="Update email address mariamgamal70.backup@gmail.com"
 
+#1.2- update password
 # change password
 change_password_xpath="//android.view.View[@content-desc='Change password']"
 change_password_accessibility_id="Change password"
 
-# allow people to follow you 
-                                  
+change_password_current_password_xpath="//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"
+change_password_new_password_xpath="//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"
+change_password_confirm_new_password_xpath="//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[3]"
+
+change_password_cancel_xpath="//android.widget.Button[@content-desc='Cancel']"
+change_password_cancel_accessibility_id="Cancel"
+change_password_save_xpath='//android.widget.Button[@content-desc="Save"]' #trying the '' instead of ""
+change_password_save_accessibility_id="Save"
+
+
+#1.3- update username
+# allow people to follow you              
 allow_people_to_follow_you_xpath="//android.view.View[@content-desc='Allow people to follow you Followers will be notified about posts you make to your profile and see them in their home feed.']"
 allow_people_to_follow_you_accessibility_id="Allow people to follow you Followers will be notified about posts you make to your profile and see them in their home feed."
+
+allow_people_to_follow_you_switch_xpath="//android.widget.Switch"
+
 
 # }
--- main.py
+++ main.py
@@ -11,48 +11,91 @@ driver.implicitly_wait(10)
 def login():
     el1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Already a redditor? Log in")
     el1.click()
-    sleep(3)
+    sleep(2)
     
     email_field=driver.find_element(AppiumBy.XPATH, value=xpath_login_email_box)
     email_field.click()
-    sleep(3)
+    sleep(2)
     email_field.send_keys("mariam")
-    driver.hide_keyboard()
-    sleep(3)
-    
-    password_field=driver.find_element(AppiumBy.XPATH, value=xpath_password_box)
-    password_field.click()
-    sleep(3)
-    password_field.send_keys("pass1234")
-    sleep(3)
     driver.hide_keyboard()
     sleep(2)
     
+    password_field=driver.find_element(AppiumBy.XPATH, value=xpath_password_box)
+    password_field.click()
+    sleep(2)
+    password_field.send_keys("pass1234")
+    sleep(2)
+    driver.hide_keyboard()
+    # sleep(2)
+    
     continue_button=driver.find_element(AppiumBy.XPATH, value=xpath_login_continue)
     continue_button.click()
-    sleep(3)
+    sleep(2)
 
-login()
 
+
+def UpvoteDownvote():
+    # el1 = driver.find_element(by=AppiumBy.XPATH, value=xpath_upvote_34_post)
+    # el1.click()
+    # sleep(2)
     
-# def recorded_login():
-#     el1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Already a redditor? Log in")
-#     el1.click()
-#     el2 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"android.widget.EditText\").instance(0)")
-#     el2.send_keys("mariam")
-#     sleep(3)
-#     el3 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"android.widget.EditText\").instance(1)")
-#     el3.send_keys("pass1234")
-#     el4 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Conitnue")
-#     # el4.click()
-#     el4.click()
-#     sleep(3)
-#     # el4.send_keys("")
-#     el5 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Conitnue")
-#     el5.click()
-# # el5.clear()
-# recorded_login()
+    el2 = driver.find_element(by=AppiumBy.XPATH, value=xpath_upvote_34_post)
+    el2.click()
+    sleep(1)
+    
+    el3 = driver.find_element(by=AppiumBy.XPATH, value=xpath_downvote_1_post)
+    el3.click()
+    sleep(1)
+    
+    el4 = driver.find_element(by=AppiumBy.XPATH, value=xpath_34_1_post_settings)
+    el4.click()
+    sleep(1)
 
 
 
 
+def logout(): #from homepage, profile_tab, tested, CORRECT
+    profile_tab=driver.find_element(AppiumBy.XPATH, value=xpath_to_profile_tab)
+    profile_tab.click()
+    sleep(2)
+    
+    logout=driver.find_element(AppiumBy.XPATH, value=logout_in_list_xpath)
+    logout.click()
+    sleep(1)
+
+def LoginandOut(): #tested, CORRECT
+    login()
+    logout()
+    
+
+
+def clickProfileTabHome(): #called from Home
+    profile_tab=driver.find_element(AppiumBy.XPATH, value=xpath_to_profile_tab)
+    profile_tab.click()
+    sleep(1)
+
+def goToSettings(): #called from Home
+    # login()
+    clickProfileTabHome()
+    settings=driver.find_element(AppiumBy.XPATH, value=settings_xpath)
+    settings.click()
+    sleep(1)
+    
+    
+    
+
+# LoginandOut() # tested, correct
+
+# logout()
+login()
+# goToSettings() #
+
+
+
+
+
+
+
+
+
+# driver.quit()
