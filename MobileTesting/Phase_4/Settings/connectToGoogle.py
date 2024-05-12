e92c49b402212eea7c6f0ce1dd2d61391b324311
Mohamed Gamal <mg68163@gmail.com>
2024-05-12 11:43:26 +0300

    committing last tests

--- MobileTesting/Phase_4/Settings/connectToGoogle.py
+++ MobileTesting/Phase_4/Settings/connectToGoogle.py
@@ -4,10 +4,5 @@ sys.path.append('E:\Spring2024\SW\Phase3\mobile_tests\MobileTesting\Phase_4')
 from main import *
 
 
-def assert_element(element, message):
-    sleep(0.5)
-    try:
-        assert element is not None, message
-    except AssertionError as e:
-        print(e)
-    return element
+
+
